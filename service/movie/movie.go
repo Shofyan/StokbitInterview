9bf26ac9b93a7f561775db59bb30d0ce818bd9de
shofyan <pencari.angin1@gmail.com>
2021-12-24 20:10:50 +0700

    update clean

--- service/movie/movie.go
+++ service/movie/movie.go
@@ -5,13 +5,10 @@ import (
 )
 
 func (s *SvcMovie) GetOneMovie(imdbID string) repository.Movie {
-	var m repository.Movie
-	m = s.RepoMovie.GetOneMovie(imdbID)
-	return m
+	return s.RepoMovie.GetOneMovie(imdbID)
 }
 
 func (s *SvcMovie) SearchMovie(key string, page string) repository.SeachMovie {
-	var m repository.SeachMovie
-	m = s.RepoMovie.SearchMovie(key, page)
-	return m
+
+	return s.RepoMovie.SearchMovie(key, page)
 }
